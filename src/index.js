8de55fac9bb302dcf84c3c9050e1df3fad4da6df
FRANCOIZER <round.francoiz@gmail.com>
2021-05-16 17:28:15 -0500

    Banner Native

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import './index.css';
 import SlideDestinos from './components/SlideDestinos';
 import NavBar from './components/NavBar';
 import Nav from './components/Nav';
+import Banner1Nat from './components/Banner1Nat'
 
 const container =  document.getElementById('app');
 
-reactDOM.render(< NavBar />,container);
+reactDOM.render(< Banner1Nat />,container);
