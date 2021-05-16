60849cd1dda240f0c55ea79cd0222d94c7dcb0d4
daniel08022 <74110809+DanielE0802@users.noreply.github.com>
2021-05-15 19:07:14 -0500

    index

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import reactDOM from 'react-dom';
 import './index.css';
-import SlideDestinos from './components/SlideDestinos';
-import NavBar from './components/NavBar';
-import Nav from './components/Nav';
 import Home from './pages/Home';
 
 const container =  document.getElementById('app');
