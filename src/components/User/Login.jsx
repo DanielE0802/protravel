f99b343acb5dc6472ee1c4116468193c620a3c47
oscar2620 <74290676+oscar2620@users.noreply.github.com>
2021-05-19 15:39:49 -0500

    login and register finished

--- src/components/User/Login.jsx
+++ src/components/User/Login.jsx
@@ -1,61 +1,131 @@
 import React from 'react';
-import '../scss/Login.css';
-import Imagen1 from '../../img/LoginIMG.jpg';
-import { Button, TextField, Grid } from '@material-ui/core';
-import Back from '../Back';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Paper from '@material-ui/core/Paper';
+import Box from '@material-ui/core/Box';
+import Grid from '@material-ui/core/Grid';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
 
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://material-ui.com/">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
 
-class LogIn extends React.Component {
-    render() {
-        return (
-            <Grid container className="contenedor">
-                <Grid item xs={0} md={5}>
-                    <img className="imagen" src={Imagen1} alt="" />
-                </Grid>
+const useStyles = makeStyles((theme) => ({
+  root: {
+    height: '100vh',
+  },
+  image: {
+    backgroundImage: 'url(https://i.pinimg.com/originals/bb/e3/47/bbe347e350c0d3975d68ad52a641575f.jpg)',
+    backgroundRepeat: 'no-repeat',
+    backgroundColor:
+      theme.palette.type === 'light' ? theme.palette.grey[50] : theme.palette.grey[900],
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+  },
+  paper: {
+    margin: theme.spacing(8, 4),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
 
+export default function SignInSide() {
+  const classes = useStyles();
 
-                <Grid className="texto" item xs={12} md={7 }>
-                <Grid style={{ transform: "translate(-300px, 10px)" }}>
-                    <Back />
-                </Grid>
-                    <form >
-                        <h2>Ingresa con tu cuenta</h2>
-                        <h4>Es fácil y rápido</h4>
-                        <h4 className="TextoInput">Correo electronico</h4>
-                        <TextField
-                            label="Ingrese su correo"
-                            color="primary"
-                            className="InputMaterial correo"
-                            variant="outlined"
-                            type="email"
-                        />
-                        <h4 className="TextoInput">Contraseña</h4>
-                        <TextField
-                            label="Ingrese su contraseña"
-                            color="primary"
-                            className="contraseña"
-                            type="password"
-                            variant="outlined"
-                        />
-                    </form>
-                    <div>
-                        <a href="/contraseña"><h6 className="Recuperar_contraseña">Olvidé mi contraseña</h6></a>
-                    </div>
-                    <br />
-                    <Button variant="contained" color="primary">
-                        Ingresar
-                        </Button>
-                    <Button className="boton" variant="contained" color="primary" href="/register">
-                        Registrarse
-                        </Button>
-                </Grid>
-
-                
+  return (
+    <Grid container component="main" className={classes.root}>
+      <CssBaseline />
+      <Grid item xs={false} sm={4} md={7} className={classes.image} />
+      <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+        <div className={classes.paper}>
+          <Avatar className={classes.avatar}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <form className={classes.form} noValidate>
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+            >
+              Sign In
+            </Button>
+            <Grid container>
+              <Grid item xs>
+                <Link href="#" variant="body2">
+                  Forgot password?
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link href="#" variant="body2">
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
             </Grid>
-
-
-        );
-    };
-};
-
-export default LogIn;
+            <Box mt={5}>
+              <Copyright />
+            </Box>
+          </form>
+        </div>
+      </Grid>
+    </Grid>
+  );
+}
