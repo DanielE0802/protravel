843ef9a4d7ccb1835259e5d7cd406b86f91cc9a7
daniel08022 <74110809+DanielE0802@users.noreply.github.com>
2021-05-19 13:06:52 -0500

    Section desnity and fix error

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,45 +1,48 @@
-import React from 'react'
-import AppBar from '@material-ui/core/AppBar'
-import Toolbar from '@material-ui/core/Toolbar'
-import Typography from '@material-ui/core/Typography'
-import { makeStyles, IconButton, MuiThemeProvider } from '@material-ui/core'
-import theme from './theme/themeconfig'
-import MenuIcon from '@material-ui/icons/Menu'
-import Lista from './Lista'
-import LanguageIcon from '@material-ui/icons/Language';
-import clsx from 'clsx';
-import Drawer from '@material-ui/core/Drawer';
-import Hidden from '@material-ui/core/Hidden';
-import SwipeableDrawer from '@material-ui/core/SwipeableDrawer';
+import React from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles, IconButton, MuiThemeProvider } from "@material-ui/core";
+import theme from "./theme/themeconfig";
+import MenuIcon from "@material-ui/icons/Menu";
+import Lista from "./Lista";
+import LanguageIcon from "@material-ui/icons/Language";
+import clsx from "clsx";
+// import Drawer from '@material-ui/core/Drawer';
+import Logo from "../img/mundo.png";
+import Hidden from "@material-ui/core/Hidden";
+import SwipeableDrawer from "@material-ui/core/SwipeableDrawer";
 
+const myStyleA = {
+  textDecoration: "none",
+  color: "#fff",
+};
 
 const useStyles = makeStyles({
   offset: theme.mixins.toolbar,
-  bar:{
-      flexGrow:1
+  bar: {
+    flexGrow: 1,
   },
   list: {
     width: 250,
   },
   fullList: {
-    width: 'auto',
+    width: "auto",
   },
-  margin:{
-    marginLeft:'15px',
-   
+  margin: {
+    marginLeft: "15px",
   },
 
-  cajaMargin:{
-    marginRight: '50px',
+  cajaMargin: {
+    marginRight: "50px",
   },
-  cajaPadding:{
-    paddingRight: '20px',
-    paddingLeft: '20px'
-  }
+  cajaPadding: {
+    paddingRight: "20px",
+    paddingLeft: "20px",
+  },
 });
 
 export default function Nav() {
-
   const classes = useStyles();
   const [state, setState] = React.useState({
     top: false,
@@ -49,7 +52,11 @@ export default function Nav() {
   });
 
   const toggleDrawer = (anchor, open) => (event) => {
-    if (event && event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+    if (
+      event &&
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
+    ) {
       return;
     }
 
@@ -59,7 +66,7 @@ export default function Nav() {
   const list = (anchor) => (
     <div
       className={clsx(classes.list, {
-        [classes.fullList]: anchor === 'top' || anchor === 'bottom',
+        [classes.fullList]: anchor === "top" || anchor === "bottom",
       })}
       role="presentation"
       onClick={toggleDrawer(anchor, false)}
@@ -71,74 +78,61 @@ export default function Nav() {
 
   return (
     <MuiThemeProvider theme={theme}>
-    <div>
       <div>
-<AppBar position="fixed" color="primary" className={classes.cajaPadding}>
-  <Toolbar>
-    <Typography variant="h6" className={classes.bar}>
-      Logo
-    </Typography>
-    <div >
-    <Hidden xsDown>
-    <IconButton color="inherit" className={classes.cajaMargin} >
-    <LanguageIcon/>
-    
-    <Typography variant="h6" className={classes.bar, classes.margin}>
-    Unete a nosotros
-  </Typography>
-
-  </IconButton>
-  </Hidden>
-    {["right"].map(() => (
-        <React.Fragment key={"right"}>
-          <IconButton color="inherit" onClick={toggleDrawer("right", true)}>{<MenuIcon/>}</IconButton>
-          <SwipeableDrawer
-            anchor={"right"}
-            open={state["right"]}
-            onClose={toggleDrawer("right", false)}
-            onOpen={toggleDrawer("right", true)}
+        <div>
+          <AppBar
+            position="fixed"
+            color="primary"
+            className={classes.cajaPadding}
           >
-            {list("right")}
-          </SwipeableDrawer>
-        </React.Fragment>
-      ))}
-    </div>
-    </Toolbar>
-      </AppBar>
-      <div className={classes.offset}>
-      </div>
-      </div>
-      
-      
-    </div>
-    </MuiThemeProvider>
+            <Toolbar>
+              <a href="/" className={classes.bar} style={myStyleA}>
+                <Typography variant="h6" style={{ display: "flex" }}>
+                  <img
+                    style={{ width: "45px", height: "45px" }}
+                    src={Logo}
+                    alt=""
+                  />
+                  <p style={{ margin: "10px" }}>ProTravel</p>
+                </Typography>
+              </a>
+              <div>
+                <Hidden xsDown>
+                  <IconButton color="inherit" className={classes.cajaMargin}>
+                    <LanguageIcon />
 
+                    <Typography
+                      variant="h6"
+                      className={(classes.bar, classes.margin)}
+                    >
+                      Unete a nosotros
+                    </Typography>
+                  </IconButton>
+                </Hidden>
+                {["right"].map(() => (
+                  <React.Fragment key={"right"}>
+                    <IconButton
+                      color="inherit"
+                      onClick={toggleDrawer("right", true)}
+                    >
+                      {<MenuIcon />}
+                    </IconButton>
+                    <SwipeableDrawer
+                      anchor={"right"}
+                      open={state["right"]}
+                      onClose={toggleDrawer("right", false)}
+                      onOpen={toggleDrawer("right", true)}
+                    >
+                      {list("right")}
+                    </SwipeableDrawer>
+                  </React.Fragment>
+                ))}
+              </div>
+            </Toolbar>
+          </AppBar>
+          <div className={classes.offset}></div>
+        </div>
+      </div>
+    </MuiThemeProvider>
   );
 }
-
-   
-
-// {/* const Nav = () => {
-//     const classes = useStyles()
-//     return (
-               
-//               </Toolbar>
-//             </AppBar>
-//             <div className={classes.offset}>
-//             </div>
-
-//             {["right"].map(() => (
-//           <React.Fragment key={"right"}>
-//             <Button onClick={toggleDrawer("right", true)}>{"right"}</Button>
-//             <Drawer anchor={"right"} open={state["right"]} onClose={toggleDrawer("right", false)}>
-//               {list("right")}
-//             </Drawer>
-//           </React.Fragment>
-//         ))}
-//         </div>
-// //     )
-// // } */}
-
-
-
-
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -3,13 +3,16 @@ import NavBar from '../components/NavBar'
 import {ThemeProvider} from '@material-ui/core/styles'
 // // import theme from '../components/theme/themeconfig'
 // import Contenedor from '../components/Contenedor'
-// import SlidesDestinos from '../components/SlideDestinos'
+import SlidesDestinos from '../components/SlideDestinos'
+import SwipeableTextMobileStepper from '../components/SlideTargets'
+import SectionsDestiny from '../components/SectionsDestiny'
 
 const Home = () => {
     return (
         <ThemeProvider>
             <NavBar></NavBar>
-
+            <SwipeableTextMobileStepper/>
+            <SectionsDestiny/>
         </ThemeProvider>
     )
 }
