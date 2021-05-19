b0c68d334b8870458d8fa04aa97c3465f4cc144f
oscar2620 <74290676+oscar2620@users.noreply.github.com>
2021-05-19 12:31:38 -0500

    Selector destiny

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import NavBar from '../components/NavBar'
 import {ThemeProvider} from '@material-ui/core/styles'
 import MainSelector from '../components/MainSelector';
+import Destinos from '../components/Destinos';
 // // import theme from '../components/theme/themeconfig'
 // import Contenedor from '../components/Contenedor'
 // import SlidesDestinos from '../components/SlideDestinos'
@@ -11,6 +12,7 @@ const Home = () => {
         <ThemeProvider>
             <NavBar></NavBar>
             <MainSelector />
+            <Destinos />
         </ThemeProvider>
     )
 }
