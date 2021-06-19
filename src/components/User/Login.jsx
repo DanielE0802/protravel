73e5de52bd3ae7ac5b4951526679292df5ef12b2
oscar2620 <74290676+oscar2620@users.noreply.github.com>
2021-06-19 12:56:10 -0500

    Colors

--- src/components/User/Login.jsx
+++ src/components/User/Login.jsx
@@ -12,6 +12,8 @@ import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Back from '../Back';
+import '../scss/user.scss';
+
 
 
 
